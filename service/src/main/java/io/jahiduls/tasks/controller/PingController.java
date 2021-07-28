ccc43d27fc8a012dd2ec55a200798fae4c5fd045
Jahid <jahid90@users.noreply.github.com>
2021-07-28 21:46:01 +0530

    [service] add ping controller

---
+++ service/src/main/java/io/jahiduls/tasks/controller/PingController.java
@@ -0,0 +1,19 @@
+package io.jahiduls.tasks.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String healthCheck() {
+        return "OK";
+    }
+
+    @GetMapping("/deep-ping")
+    public String deepHealthCheck() {
+        return "OK";
+    }
+
+}
