030faf87413af1e25ab7af3e60524d4cf31f1881
Jahid <jahid90@users.noreply.github.com>
2022-01-16 19:38:37 +0530

    [client] add eslint and prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import Workspaces from './components/Workspaces';
 
 function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        Tasks App
-      </header>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <header className='app-header'>Tasks App</header>
+            <Workspaces />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Workspaces.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Workspaces = () => {
+    return <div>List of workspaces:</div>;
+};
+
+export default Workspaces;
