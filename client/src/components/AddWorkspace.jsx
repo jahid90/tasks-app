8e65e3d5019e252fbfd21d743515e34d451ca464
Jahid <jahid90@users.noreply.github.com>
2022-01-17 19:38:17 +0530

    [client] bind variables to name and description during workspace creation

--- client/src/components/AddWorkspace.jsx
+++ client/src/components/AddWorkspace.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Form } from 'react-bootstrap';
 
-function AddWorkspace() {
+const AddWorkspace = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(`submitted: name=${name} description=${description}`);
+
+        setName('');
+        setDescription('');
+    };
+
     return (
         <Col>
             <h3>Create a workspace</h3>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className='mb-3' controlId='name'>
                     <Form.Label>Workspace Name</Form.Label>
-                    <Form.Control type='text' placeholder='Enter workspace name' />
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter workspace name'
+                        value={name}
+                        onChange={handleNameChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className='mb-3' controlId='description'>
                     <Form.Label>Description</Form.Label>
-                    <Form.Control type='text' placeholder='Enter a description' />
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter a description'
+                        value={description}
+                        onChange={handleDescriptionChange}
+                    />
                 </Form.Group>
 
                 <Button variant='primary' type='submit'>
@@ -22,6 +52,6 @@ function AddWorkspace() {
             </Form>
         </Col>
     );
-}
+};
 
 export default AddWorkspace;
